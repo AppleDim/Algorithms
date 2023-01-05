974707519ff8c5b6528b76a0cfa78a84956d21a3
Dima Polyakov <woodhill@bk.com>
2023-01-05 22:07:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * @created ${DAY}.${MONTH}.${YEAR} ${TIME}
+ * @author Polyakov Dmitry
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
