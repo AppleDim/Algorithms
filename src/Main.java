13a9478bd2ac220d6fb3e7d875b63fb03a678df7
Dima Polyakov <woodhill@bk.com>
2023-01-05 22:33:17 +0300

    Task 1

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * @created ${DAY}.${MONTH}.${YEAR} ${TIME}
- * @author Polyakov Dmitry
-*/public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
